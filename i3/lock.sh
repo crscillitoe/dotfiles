5841214589373da67ae5153c33b8272dc80e0100
crscillitoe <crscillitoe@wpi.edu>
2017-08-04 18:40:23 -0400

    Updated tmux conf to be gnome-friendly.

--- i3/lock.sh
+++ i3/lock.sh
@@ -1,6 +1,6 @@
 tmpbg=`mktemp /tmp/lockscreen.XXXXXXXXXX.png` || exit 1
 
 scrot "$tmpbg"
-convert "$tmpbg" -scale 10% -scale 1000% "$tmpbg"
+convert "$tmpbg" -scale 20% -scale 500% "$tmpbg"
 i3lock -i "$tmpbg"
 rm "$tmpbg"
