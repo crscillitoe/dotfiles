50508d3da5a92b599cf6297af67e05791caf7418
crscillitoe <crscillitoe@wpi.edu>
2018-09-21 10:35:05 -0400

    updated tmux installer

--- tmux/install.sh
+++ tmux/install.sh
@@ -1,5 +1,4 @@
-#cp /home/christian/.tmux.conf /home/christian/.tmux.conf.OLD
-#cp /home/christian/.tmux.conf.local ~/.tmux.conf.local.OLD
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+cp tmux.conf.new ~/.tmux.conf
 
-cp tmux.conf /home/christian/.tmux.conf
-cp tmux.conf.local /home/christian/.tmux.conf.local
+echo "Install plugins with Ctrl+a --> I"
